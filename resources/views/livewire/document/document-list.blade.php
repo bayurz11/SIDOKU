6169e217b6fbe99397b638a32524917b43d87091
Bayu <bayurz1001@gmail.com>
2025-11-14 00:02:01 +0700

    Feat : Documen  setting

--- resources/views/livewire/document/document-list.blade.php
+++ resources/views/livewire/document/document-list.blade.php
@@ -29,8 +29,7 @@
                 {{-- Import Document --}}
                 @permission('documents.import')
                     <button wire:click="$dispatch('openDocumentForm')"
-                        class="group border-2 border-green-600 text-green-700 bg-white 
- hover:bg-gradient-to-r hover:from-green-600 hover:to-purple-600 hover:text-white px-6 py-3 rounded-xl text-sm font-semibold flex items-center transition-all duration-300 hover:shadow-xl hover:scale-105">
+                        class="group border-2 border-green-600 text-green-700 bg-green hover:bg-gradient-to-r hover:from-green-600 hover:to-purple-600 hover:text-white px-6 py-3 rounded-xl text-sm font-semibold flex items-center transition-all duration-300 hover:shadow-xl hover:scale-105">
 
                         <svg class="w-5 h-5 mr-2 transition-transform duration-300 group-hover:rotate-90" fill="none"
                             stroke="currentColor" viewBox="0 0 24 24">
@@ -260,7 +259,7 @@
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414
-                                                                                                            a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                                                                                                                a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
                                             </path>
                                         </svg>
                                         Edit
@@ -290,7 +289,7 @@
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6
-                                                                                                            m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
+                                                                                                                m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
                                         Delete
                                     </button>
