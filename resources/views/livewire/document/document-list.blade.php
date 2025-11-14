2e8b0efe5776b7579671950e003202849c5097b8
Bayu <bayurz1001@gmail.com>
2025-11-14 20:57:39 +0700

    Sytle : Documen  setting

--- resources/views/livewire/document/document-list.blade.php
+++ resources/views/livewire/document/document-list.blade.php
@@ -258,7 +258,7 @@
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414
-                                                                                                                                                    a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                                                                                                                                                        a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
                                             </path>
                                         </svg>
                                         Edit
@@ -269,7 +269,7 @@
                                     {{-- Mark obsolete --}}
                                     @if ($doc->status !== 'obsolete')
                                         <button wire:click="markObsolete({{ $doc->id }})"
-                                            class="inline-flex items-center px-3 py-2 text-xs font-semibold text-Yellow-700 bg-red-50 rounded-lg hover:bg-Yellow-100 hover:text-Yellow-800 transition-all duration-200">
+                                            class="inline-flex items-center px-3 py-2 text-xs font-semibold text-pink-700 bg-red-50 rounded-lg hover:bg-red-100 hover:text-pink-800 transition-all duration-200">
                                             <svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor"
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -288,7 +288,7 @@
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6
-                                                                                                                                                    m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
+                                                                                                                                                        m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
                                         Delete
                                     </button>
