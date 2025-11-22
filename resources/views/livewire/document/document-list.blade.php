69e38aef3bfa7156a8f19bc79f5ae1acc6863bb7
Bayu <bayurz1001@gmail.com>
2025-11-22 21:45:00 +0700

    fate : import document  setting

--- resources/views/livewire/document/document-list.blade.php
+++ resources/views/livewire/document/document-list.blade.php
@@ -26,8 +26,8 @@
             <div class="flex items-center gap-2">
                 @permission('documents.import')
                     <button wire:click="$dispatch('openDocumentImportForm')"
-                        class="group bg-gradient-to-r from-blue-600 to-purple-600
-               hover:from-blue-700 hover:to-purple-700
+                        class="group bg-gradient-to-r from-green-600 to-purple-600
+               hover:from-green-700 hover:to-purple-700
                text-white px-6 py-3 rounded-xl text-sm font-semibold
                inline-flex items-center shadow-lg hover:shadow-xl
                transition-all duration-300 transform hover:scale-105">
@@ -259,7 +259,7 @@
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414
-                                                                                                                                                                                                                    a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                                                                                                                                                                                                                        a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
                                             </path>
                                         </svg>
                                         Edit
@@ -289,7 +289,7 @@
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6
-                                                                                                                                                                                                                    m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
+                                                                                                                                                                                                                        m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
                                         Delete
                                     </button>
