24f5f77c6a644abb14d03573363d386f418ebd52
Bayu <bayurz1001@gmail.com>
2025-11-22 22:54:15 +0700

    fate : import document  setting

--- resources/views/livewire/document/document-import-form.blade.php
+++ resources/views/livewire/document/document-import-form.blade.php
@@ -86,7 +86,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1.5">
                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M12 16.5V9.75m0 6.75 2.25-2.25M12 16.5l-2.25-2.25M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25v-6.63a2.25 2.25 0 0 0-.659-1.591l-6.87-6.87A2.25 2.25 0 0 0 12.63 1.5H6.75A2.25 2.25 0 0 0 4.5 3.75v13.5z" />
+                                            d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                     </svg>
                                     Download Template
                                 </a>
