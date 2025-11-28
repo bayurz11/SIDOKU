b96c3d716934d5406365cddd8f6ae1239fc56350
Bayu <bayurz1001@gmail.com>
2025-11-28 21:08:29 +0700

    Adds document viewing feature

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -5,5 +5,6 @@
         <livewire:document.document-list />
         <livewire:document.document-form />
         <livewire:document.document-import-form />
+        <livewire:document.document-detail-form />
     </div>
 @endsection
