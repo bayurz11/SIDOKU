edbeed5929f16ece8f25cad78cdfa6f524a8d41f
Bayu <bayurz1001@gmail.com>
2025-11-28 21:44:19 +0700

    update : document list

--- resources/views/livewire/document/document-detail-form.blade.php
+++ resources/views/livewire/document/document-detail-form.blade.php
@@ -267,9 +267,8 @@
                                 <svg class="w-4 h-4 mr-1.5" xmlns="http://www.w3.org/2000/svg" fill="none"
                                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                                        d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
                                 </svg>
-
                                 Ajukan Dokumen
                             </button>
                         @endif
