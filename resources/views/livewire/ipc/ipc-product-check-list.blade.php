6ed1ffeb405423aff2a9ea2bc9e1e89163fae20e
Bayu <bayurz1001@gmail.com>
2025-12-02 23:50:18 +0700

    fate : IPC List and Form

--- resources/views/livewire/ipc/ipc-product-check-list.blade.php
+++ resources/views/livewire/ipc/ipc-product-check-list.blade.php
@@ -174,9 +174,9 @@
                     </div>
 
                     <div>
-                        <label class="block text-xs font-semibold text-gray-600 mb-1">Sub Line @if ($filterLineGroup !== 'LINE_TEH')
+                        <label class="block text-xs font-semibold text-gray-600 mb-1">Sub Line( @if ($filterLineGroup !== 'LINE_TEH')
                                 Aktif jika Line Group = Line Teh
-                            @endif
+                            @endif)
                         </label>
                         <select wire:model.live="filterSubLine"
                             class="block w-full px-3 py-2 border border-gray-300 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"
@@ -335,7 +335,7 @@
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414
-                                                                                                                a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                                                                                                                    a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
                                                 </path>
                                             </svg>
                                             Edit
@@ -349,7 +349,7 @@
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6
-                                                                                                                m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
+                                                                                                                    m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
                                             </svg>
                                             Delete
                                         </button>
