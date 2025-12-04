9861ddf9f69ad7d5eaf7cf3f7c061afb9baa1daa
Bayu <bayurz1001@gmail.com>
2025-12-04 22:44:44 +0700

    Fate : Tiup Botol List

--- resources/views/tiup-botol/index.blade.php
+++ resources/views/tiup-botol/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="space-y-6">
-        {{-- <livewire:ipc.ipc-product-check-list />
-        <livewire:ipc.ipc-product-check-form />
+        <livewire:ipc.tiup-botol-list />
+        {{-- <livewire:ipc.ipc-product-check-form />
         <livewire:ipc.ipc-product-detail />
         <livewire:ipc.ipc-product-import /> --}}
     </div>
