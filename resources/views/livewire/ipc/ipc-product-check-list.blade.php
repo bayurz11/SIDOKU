943390a94f4a28db2cc94248cf51ee7dd5540649
Bayu <bayurz1001@gmail.com>
2025-12-06 23:12:42 +0700

    Update : Display test date for high moisture items in product check list

--- resources/views/livewire/ipc/ipc-product-check-list.blade.php
+++ resources/views/livewire/ipc/ipc-product-check-list.blade.php
@@ -93,7 +93,7 @@
                                             @endif
                                             → <strong>{{ round($row->avg_moisture, 2) }}%</strong>
                                             <span class="ml-1 text-red-600">
-                                                ({{ \Carbon\Carbon::parse($row->test_date)->format('d M Y') }})
+                                                {{ optional($row->test_date)->format('d M Y') }}
                                             </span>
                                         </li>
                                     @endforeach
@@ -380,7 +380,7 @@
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414
-                                                                                                                                                                                                                                                                                                                                                                    a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                                                                                                                                                                                                                                                                                                                                                                        a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
                                                 </path>
                                             </svg>
                                             Edit
@@ -394,7 +394,7 @@
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6
-                                                                                                                                                                                                                                                                                                                                                                    m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
+                                                                                                                                                                                                                                                                                                                                                                        m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
                                             </svg>
                                             Delete
                                         </button>
