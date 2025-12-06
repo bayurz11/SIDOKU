77368f563890b8e5d7edd9ec9d2b50c193ab2845
Bayu <bayurz1001@gmail.com>
2025-12-06 22:08:18 +0700

    Update : Navbar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -192,63 +192,61 @@
 
                      </div>
                  </div>
-
-             </div>
-
-             {{-- ACCOUNT --}}
-             <div class="relative pt-4 mt-4 border-t border-blue-400 border-opacity-30">
-                 <button @click="activeMenu = (activeMenu === 'account' ? '' : 'account')"
-                     aria-controls="menu-account" :aria-expanded="(activeMenu === 'account').toString()"
-                     class="group relative flex w-full items-center rounded-lg px-4 py-3 text-sm font-medium transition-colors duration-200
+                 {{-- ACCOUNT --}}
+                 <div class="relative pt-4 mt-4 border-t border-blue-400 border-opacity-30">
+                     <button @click="activeMenu = (activeMenu === 'account' ? '' : 'account')"
+                         aria-controls="menu-account" :aria-expanded="(activeMenu === 'account').toString()"
+                         class="group relative flex w-full items-center rounded-lg px-4 py-3 text-sm font-medium transition-colors duration-200
                {{ $accountActive ? 'text-white' : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}"
-                     type="button">
-                     @if ($accountActive)
-                         <span class="absolute left-0 top-1/2 h-6 w-1 -translate-y-1/2 rounded-r bg-emerald-400">
-                         </span>
-                     @endif
+                         type="button">
+                         @if ($accountActive)
+                             <span class="absolute left-0 top-1/2 h-6 w-1 -translate-y-1/2 rounded-r bg-emerald-400">
+                             </span>
+                         @endif
 
-                     {{-- ICON ACCOUNT (bisa diganti sesuai selera) --}}
-                     <svg class="mr-3 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                             d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
-                         <path stroke-linecap="round" stroke-linejoin="round"
-                             d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                     </svg>
+                         {{-- ICON ACCOUNT (bisa diganti sesuai selera) --}}
+                         <svg class="mr-3 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                 d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                             <path stroke-linecap="round" stroke-linejoin="round"
+                                 d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                         </svg>
 
-                     <span>Users & Roles</span>
+                         <span>Users & Roles</span>
 
-                     {{-- CHEVRON --}}
-                     <svg :class="{ 'rotate-180': activeMenu === 'account' }"
-                         class="ml-auto h-4 w-4 transform transition-transform duration-200" fill="none"
-                         stroke="currentColor" viewBox="0 0 24 24">
-                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                     </svg>
-                 </button>
+                         {{-- CHEVRON --}}
+                         <svg :class="{ 'rotate-180': activeMenu === 'account' }"
+                             class="ml-auto h-4 w-4 transform transition-transform duration-200" fill="none"
+                             stroke="currentColor" viewBox="0 0 24 24">
+                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                 d="M19 9l-7 7-7-7" />
+                         </svg>
+                     </button>
 
-                 <div id="menu-account" x-show="activeMenu === 'account'" x-collapse
-                     class="mt-1 pl-10 space-y-1 overflow-hidden">
-                     @permission('users.view')
-                         <a href="{{ route('users.index') }}"
-                             class="block rounded-md px-4 py-2 text-sm
+                     <div id="menu-account" x-show="activeMenu === 'account'" x-collapse
+                         class="mt-1 pl-10 space-y-1 overflow-hidden">
+                         @permission('users.view')
+                             <a href="{{ route('users.index') }}"
+                                 class="block rounded-md px-4 py-2 text-sm
                {{ request()->routeIs('users.*')
                    ? 'bg-white bg-opacity-20 text-white'
                    : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}">
-                             Users
-                         </a>
-                     @endpermission
+                                 Users
+                             </a>
+                         @endpermission
 
-                     @permission('roles.view')
-                         <a href="{{ route('roles.index') }}"
-                             class="block rounded-md px-4 py-2 text-sm
+                         @permission('roles.view')
+                             <a href="{{ route('roles.index') }}"
+                                 class="block rounded-md px-4 py-2 text-sm
                {{ request()->routeIs('roles.*')
                    ? 'bg-white bg-opacity-20 text-white'
                    : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}">
-                             Roles & Permissions
-                         </a>
-                     @endpermission
+                                 Roles & Permissions
+                             </a>
+                         @endpermission
+                     </div>
                  </div>
              </div>
-
          </div>
      </nav>
 
