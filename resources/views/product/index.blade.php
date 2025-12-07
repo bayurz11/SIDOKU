0b609880e4860fe0aca73fde0f77ab039d51199a
Bayu <bayurz1001@gmail.com>
2025-12-07 21:37:29 +0700

    feat: implement IPC product management with detailed views and forms

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,7 +4,7 @@
     <div class="space-y-6">
         <livewire:ipc.in-preces-controlel-form />
         <livewire:ipc.in-preces-controlel-list />
-        {{-- <livewire:ipc.ipc-product-detail />
-        <livewire:ipc.ipc-product-import /> --}}
+        <livewire:ipc.in-proces-controlel-detail />
+        {{-- <livewire:ipc.ipc-product-import /> --}}
     </div>
 @endsection
