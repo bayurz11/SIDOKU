8528fa66386b394e142d6e980c117ced6fa1168b
Bayu <bayurz1001@gmail.com>
2025-12-07 21:53:48 +0700

    feat: enhance Tiup Botol detail view with modal and data display

--- resources/views/tiup-botol/index.blade.php
+++ resources/views/tiup-botol/index.blade.php
@@ -4,6 +4,6 @@
     <div class="space-y-6">
         <livewire:ipc.tiup-botol-list />
         <livewire:ipc.tiup-botol-form />
-        {{-- <livewire:ipc.ipc-product-detail /> --}}
+        <livewire:ipc.tiup-botol-detail />
     </div>
 @endsection
