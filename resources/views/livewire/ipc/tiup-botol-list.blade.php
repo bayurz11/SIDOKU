77787d4e253933a2fa7939c06df8229106ade03a
Bayu <bayurz1001@gmail.com>
2025-12-09 22:58:50 +0700

    Update: tiup botol

--- resources/views/livewire/ipc/tiup-botol-list.blade.php
+++ resources/views/livewire/ipc/tiup-botol-list.blade.php
@@ -231,7 +231,7 @@
                                 @endphp
                                 <span
                                     class="inline-flex items-center px-3 py-1.5 rounded-xl text-xs font-semibold border
-                                        {{ $isOk ? 'bg-blue-50 text-blue-700 border-blue-100' : 'bg-red-50 text-red-700 border-red-100' }}">
+                                        {{ $isOk ? 'bg-green-50 text-green-700 border-green-100' : 'bg-red-50 text-red-700 border-red-100' }}">
                                     {{ $label }}
                                 </span>
                             </td>
@@ -328,7 +328,7 @@
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6
-                                                                                                                                                        m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
+                                                                                                                                                            m1-10V4a1 1 0 00-1-1H9a1 1 0 00-1 1v3M4 7h16" />
                                             </svg>
                                             Delete
                                         </button>
