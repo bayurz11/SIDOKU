0b4231c3d5459f47a15c76a89dad76b28c5b89af
Bayu <bayurz1001@gmail.com>
2026-02-25 15:59:22 +0700

    Update role form

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -285,7 +285,13 @@
                              @permission('incoming_material.view')
                                  <a href="#"
                                      class="block rounded-md px-4 py-2 text-sm {{ request()->routeIs('incoming-material.*') ? 'bg-white bg-opacity-20 text-white' : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}">
-                                     Incoming Material List
+                                     Tahap 1
+                                 </a>
+                             @endpermission
+                             @permission('incoming_material.view')
+                                 <a href="#"
+                                     class="block rounded-md px-4 py-2 text-sm {{ request()->routeIs('incoming-material.*') ? 'bg-white bg-opacity-20 text-white' : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}">
+                                     Tahap 2
                                  </a>
                              @endpermission
 
