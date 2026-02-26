8292b6c5e5eb7af6158e9b4af078f09b39893ad3
Bayu <bayurz1001@gmail.com>
2026-02-26 16:28:38 +0700

    add detailed view for incoming material with modal and status display

--- app/Livewire/IncomingMaterial/IncomingMaterialDetail.php
+++ app/Livewire/IncomingMaterial/IncomingMaterialDetail.php
@@ -3,9 +3,26 @@
 namespace App\Livewire\IncomingMaterial;
 
 use Livewire\Component;
+use App\Models\Domains\IncomingMaterial\Models\IncomingMaterial;
 
 class IncomingMaterialDetail extends Component
 {
+    public $material;
+    public $showModal = false;
+
+    protected $listeners = ['openIncomingMaterialDetail'];
+
+    public function openIncomingMaterialDetail($id)
+    {
+        $this->material = IncomingMaterial::with('files')->findOrFail($id);
+        $this->showModal = true;
+    }
+
+    public function closeModal()
+    {
+        $this->reset(['material', 'showModal']);
+    }
+
     public function render()
     {
         return view('livewire.incoming-material.incoming-material-detail');
