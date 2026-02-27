78f34dc6591ad25f111cba0c2d646c27d34e6afa
Bayu <bayurz1001@gmail.com>
2026-02-27 16:10:12 +0700

    add detailed view for incoming material with modal and status display

--- resources/views/livewire/incoming-material/incoming-material-list.blade.php
+++ resources/views/livewire/incoming-material/incoming-material-list.blade.php
@@ -278,21 +278,6 @@
                                             Detail
                                         </button>
                                     @endpermission
-                                    {{-- VIEW DOKUMEN --}}
-                                    @permission('incoming_material.view')
-                                        <a href="#" target="_blank"
-                                            class="inline-flex items-center px-3 py-2 text-xs font-semibold text-purple-600 bg-purple-50 rounded-lg hover:bg-purple-100 hover:text-purple-700 transition-all duration-200">
-
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1.5" fill="none"
-                                                stroke="currentColor" viewBox="0 0 24 24" stroke-width="1.5">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                                            </svg>
-
-                                            Dokumen
-                                        </a>
-                                    @endpermission
-
 
                                     {{-- PRINT DOKUMEN --}}
                                     @permission('incoming_material.view')
@@ -318,7 +303,7 @@
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414
-                                                                                                                                                                                                                                                                             a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                                                                                                                                                                                                                                                 a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                             </svg>
                                             Edit
                                         </button>
