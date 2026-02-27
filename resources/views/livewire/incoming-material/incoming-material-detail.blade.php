b6a1964917358c9915bb881f041f8f2f0cb33fb5
Bayu <bayurz1001@gmail.com>
2026-02-27 11:43:54 +0700

    add detailed view for incoming material with modal and status display

--- resources/views/livewire/incoming-material/incoming-material-detail.blade.php
+++ resources/views/livewire/incoming-material/incoming-material-detail.blade.php
@@ -166,7 +166,7 @@
                                         Kategori: {{ strtoupper($file->category ?? '-') }}
                                     </div>
 
-                                    <a href="{{ asset(str_replace('storage/app/public/', 'storage/', $file->file_path)) }}"
+                                    <a href="{{ asset(str_replace('storage/app/public/', $file->file_path)) }}"
                                         target="_blank" class="inline-block text-blue-600 hover:underline">
                                         Lihat File
                                     </a>
