4278b6c8f1c9cb79c0e73fa833cd5e0a7553d40f
Bayu <bayurz1001@gmail.com>
2026-03-16 00:34:52 +0700

    add test parameters to IncomingMaterial model and migration

--- resources/views/livewire/incoming-material/incoming-material-list.blade.php
+++ resources/views/livewire/incoming-material/incoming-material-list.blade.php
@@ -303,7 +303,7 @@
                                                 viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414
-                                                                                                                                                                                                                                                                                                                     a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                                                                                                                                                                                                                                                                                                             a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                             </svg>
                                             Edit
                                         </button>
@@ -453,7 +453,11 @@
         });
 
         window.addEventListener('incoming-material:saved', () => {
-            location.reload();
+
+            setTimeout(() => {
+                location.reload();
+            }, 4000);
+
         });
     </script>
 @endpush
