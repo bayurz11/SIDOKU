48f52556ad10e9395de201f97695e03f33f36a33
Bayu <bayurz1001@gmail.com>
2026-04-29 11:47:54 +0700

    feat : incominig material and microbiologi module

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -149,7 +149,7 @@
                              </svg>
                          </button>
 
-                         <div id="menu-master" x-show="activeMenu === 'master'" x-collapse
+                         <div id="menu-master" x-show="activeMenu === 'master'"
                              class="mt-1 pl-10 space-y-1 overflow-hidden">
                              @permission('departments.view')
                                  <a href="{{ route('department.index') }}"
@@ -209,7 +209,7 @@
                              </svg>
                          </button>
 
-                         <div id="menu-document" x-show="activeMenu === 'document'" x-collapse
+                         <div id="menu-document" x-show="activeMenu === 'document'"
                              class="mt-1 pl-10 space-y-1 overflow-hidden">
                              @permission('documents.view')
                                  <a href="{{ route('documents.index') }}"
@@ -263,7 +263,7 @@
                              </svg>
                          </button>
 
-                         <div id="menu-ipc" x-show="activeMenu === 'ipc'" x-collapse
+                         <div id="menu-ipc" x-show="activeMenu === 'ipc'"
                              class="mt-1 pl-10 space-y-1 overflow-hidden">
 
                              @if ($canSeeIpcMoistureMenu)
@@ -307,7 +307,7 @@
 
                              <svg xmlns="http://www.w3.org/2000/svg" class="mr-3 h-5 w-5" fill="none"
                                  stroke="currentColor" viewBox="0 0 24 24">
-                                 <<path stroke-linecap="round" stroke-linejoin="round"
+                                 <path stroke-linecap="round" stroke-linejoin="round"
                                      d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
                              </svg>
 
@@ -323,8 +323,15 @@
                              </svg>
                          </button>
 
-                         <div id="menu-incoming" x-show="activeMenu === 'incoming'" x-collapse
+                         <div id="menu-incoming" x-show="activeMenu === 'incoming'"
                              class="mt-1 pl-10 space-y-1 overflow-hidden">
+
+                             @permission('incoming_material.view')
+                                 <a href="{{ route('incoming-material-items.index') }}"
+                                     class="block rounded-md px-4 py-2 text-sm {{ request()->routeIs('incoming-material-items.*') ? 'bg-white bg-opacity-20 text-white' : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}">
+                                     Master Nama Barang
+                                 </a>
+                             @endpermission
 
                              @permission('incoming_material.view')
                                  <a href="{{ route('incoming-material-tahap1.index') }}"
@@ -374,20 +381,13 @@
                              </svg>
                          </button>
 
-                         <div id="menu-microbiology" x-show="activeMenu === 'microbiology'" x-collapse
+                         <div id="menu-microbiology" x-show="activeMenu === 'microbiology'"
                              class="mt-1 pl-10 space-y-1 overflow-hidden">
 
                              @permission('microbiology.view')
                                  <a href="{{ route('microbiology.incoming-material.index') }}"
                                      class="block rounded-md px-4 py-2 text-sm {{ request()->routeIs('microbiology.incoming-material.*') ? 'bg-white bg-opacity-20 text-white' : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}">
                                      Incoming Material
-                                 </a>
-                             @endpermission
-
-                             @permission('microbiology.view')
-                                 <a href="#"
-                                     class="block rounded-md px-4 py-2 text-sm {{ request()->routeIs('microbiology-tahap2.*') ? 'bg-white bg-opacity-20 text-white' : 'text-blue-100 hover:bg-white hover:bg-opacity-10 hover:text-white' }}">
-                                     Tahap 2
                                  </a>
                              @endpermission
 
@@ -426,7 +426,7 @@
                              </svg>
                          </button>
 
-                         <div id="menu-account" x-show="activeMenu === 'account'" x-collapse
+                         <div id="menu-account" x-show="activeMenu === 'account'"
                              class="mt-1 pl-10 space-y-1 overflow-hidden">
                              @permission('users.view')
                                  <a href="{{ route('users.index') }}"
